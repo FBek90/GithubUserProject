598f144f1e98f1838377c138ab5011513f3258cd
bek <turgunboynazarov@Turguns-MacBook-Pro.local>
2021-09-06 18:35:47 +0900

    Auth0 is changed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import {Auth0Provider} from '@auth0/auth0-react'
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-pu8wyk-g.us.auth0.com"
-      clientId="GYsVn60CkUU6fa4yiwpRrT1KVL5KuzPh"
+      domain="dev-fayoz.us.auth0.com"
+      clientId="w4qux0KJDktgKlI8kUJsOngZSCejz5zH"
       redirectUri={window.location.origin}
       cacheLocation="localstorage"
     >
